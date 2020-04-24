ff5ba4c4808cce1f1a528ae204176ccf118004ee
devharshattri <57602599+devharshattri@users.noreply.github.com>
2020-04-25 04:34:51 +0530

    upload

---
+++ controllers/profile.js
@@ -0,0 +1,15 @@
+const handleProfileGet = (req, res, db) => {
+  const {id} = req.params;
+  db.select('*').from('users').where({ id : id }).then(user => {
+    if(user.length) {
+      res.json(user[0]);
+    }else {
+      res.status(404).json('Not Found');
+    }
+  })
+  .catch(err => res.status(404).json('Error getting User'));
+}
+
+module.exports = {
+	handleProfileGet: handleProfileGet
+}
---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+var knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'postgres',
+    password : 'devharsh',
+    database : 'smart-brain'
+  }
+});
+
+const app= express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req,res) => { res.send('It is working!'); });
+
+app.post('/signin', (req, res) => { signin.handleSignIn(req, res, db, bcrypt)}) ;
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt)}) ;
+
+app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
+
+app.put('/image',(req, res) => {image.handleImage(req, res, db)});
+
+app.listen(process.env.PORT || 3000, ()=> {
+	console.log(`App is running on port ${process.env.PORT}`);
+})
+
+/* 
+/--> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile:userID --> GET = user
+/image --> PUT --> user
+*/
